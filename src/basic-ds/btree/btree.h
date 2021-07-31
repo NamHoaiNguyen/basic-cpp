e1aa0f0ed70eb502b638aac8ee159c4d423443da
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2021-07-31 19:47:02 +0700

    update btree(fixing bug seg fault)

--- src/basic-ds/btree/btree.h
+++ src/basic-ds/btree/btree.h
@@ -16,10 +16,10 @@ using AcceptType = std::enable_if_t<std::is_same_v<int, U> || std::is_same_v<lon
                                     || std::is_same_v<long int, U> || std::is_same_v<long long, U>>;
 
 
-template<typename T, typename U>
-class BTree : public std::enable_shared_from_this<BTree<T, U>> {
+template<typename T, typename U, typename V>
+class BTree : public std::enable_shared_from_this<BTree<T, U, V>> {
 private:
-    std::shared_ptr<BtreeNode<T, U>> root_;
+    std::shared_ptr<BtreeNode<T, U, V>> root_;
     U size_;
 
 public:
@@ -33,50 +33,81 @@ public:
 
     void traverse();
 
-    decltype(auto) search(T const& key); 
+    // decltype(auto) search(T const& key); 
 
-    decltype(auto) add_node(T&& value);
+    decltype(auto) insert_node(T&& value);
 
     decltype(auto) split_child(T&& value);
+
+    // decltype(auto) insert_node_not_null(T&& value);
 };
 
-// template<typename T, typename U>
-// decltype(auto) BTree<T, U>::search(T const& key)
+// template<typename T, typename U, typename V>
+// decltype(auto) BTree<T, U, V>::search(T const& key)
 // {
-//     auto i = {0};
+//     auto i{0};
     
-//     while (i < node_size_ && key < node_array_[i])
+//     while (i < 2 * root_->node_size_ - 1 && key < root_->node_array_[i])
 //         i++;
     
-//     if (node_array_[i] == key)
-//         return std::shared_from_this();
+//     if (root_->node_array_[i] == key)
+//         return root_->node_array_[i];
 
-//     if (leaf_ == true)
+//     if (root_->leaf_ == true)
 //         return nullptr;
 
-//     return node_array_[i]->search(key);
+//     return root_->node_array_[i]->search(key);
 // }
 
-template<typename T, typename U>
-decltype(auto) BTree<T, U>::add_node(T&& key)
+template<typename T, typename U, typename V>
+decltype(auto) BTree<T, U, V>::insert_node(T&& key)
 {
-    // if (root_ == nullptr) {
-    //     root_ = std::make_shared<BtreeNode<T, U>>(size_, true);
-    // } 
-    // else {
-    //     if (root-> = 2 * t - 1) {
-
-    //     } 
-    //     else {
-            
-    //     }
-    // }
+    /*If root is null, create node*/
+    if (root_ == nullptr) {
+        root_ = std::make_shared<BtreeNode<T, U, V>>(std::forward<U>(size_), true);
+        root_->node_array_.get()[0] = key;
+        root_->current_numbers_key_in_node_ = 1;
+    } 
+    else {
+        /*if node is full*/
+        if (root_->current_numbers_key_in_node_ == 2 * root_->node_size_ - 1) {
+            auto btreenode = std::make_shared<BtreeNode<T, U, V>>(std::forward<U>(size_), false);
+            btreenode->child_node_[0] = root_;
+            btreenode->split_child(0, root_);
+        
+            /*New root has two child. Decide which child have new key*/
+            auto i{0};
+            if (btreenode->node_array_.get()[0] < key)    i++;
+            btreenode->child_node_[i]->insert_node_not_null(std::forward<T>(key));
+            root_ = btreenode;
+        } 
+        else {
+            root_->insert_node_not_null(std::forward<T>(key));
+        }
+    }
 }
 
-template<typename T, typename U>
-void BTree<T, U>::traverse()
+// template<typename T, typename U>
+// decltype(auto) BTree<T, U>::insert_node_not_null(T&& key)
+// {
+//     if (root_->is_leaf == true) {
+//         auto i{root_->node_size_ - 1};
+//         while (i > 0 && root_->node_array_[i] > key) {
+//             root_->node_array_[i + 1] = root_->node_array_[i];
+//             i--;
+//         } 
+//         root_->node_array_[i + 1] = key;
+//         // root
+//     }
+// }
+
+template<typename T, typename U, typename V>
+void BTree<T, U, V>::traverse()
 {
-    return;
+    if (root_ != NULL)   
+        root_->traverse();
 }
 
 #endif
+
+
--- src/basic-ds/btree/main.cpp
+++ src/basic-ds/btree/main.cpp
@@ -6,8 +6,18 @@
 
 int main()
 {
-    std::shared_ptr<BtreeNode<int, int>> node = std::make_shared<BtreeNode<int, int>>( 5, false );
-    BTree<int, int> btree(3);
-    
+    std::shared_ptr<BtreeNode<int, int, bool>> node = std::make_shared<BtreeNode<int, int, bool>>( 5, false );
+    BTree<int, int, bool> t(3);
+    t.insert_node(10);
+    // t.insert_node(20);
+    // t.insert_node(5);
+    // t.insert_node(6);
+    // t.insert_node(12);
+    // t.insert_node(30);
+    // t.insert_node(7);
+    // t.insert_node(17);
+  
+    std::cout << "Traversal of the constucted tree is ";
+    t.traverse();  
     return 0;
 }
