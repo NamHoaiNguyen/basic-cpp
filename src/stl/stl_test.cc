110e47de078de0e4b397875b69c8fa90fe20d074
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2021-08-21 00:17:35 +0700

    test stl

--- src/stl/stl_test.cc
+++ src/stl/stl_test.cc
@@ -3,9 +3,41 @@
 #include "utils.h"
 
 #include <boost/test/included/unit_test.hpp>
+#include <iostream>
+#include <string_view>
+#include <vector>
 
+void log(std::string_view txt, int64_t val, bool tag) {
+	if (tag) {
+		std::cout << txt << std::boolalpha << (bool)val << "\n";
+	} else {
+		std::cout << txt << std::noboolalpha << val << "\n";
+	}
+}
+
+/*test any_of, all_of, none_of*/
 BOOST_AUTO_TEST_CASE(test_algorithm)
 {
    TEST_LOG();
-    // std::cout << boost::unit_test::framework::current_test_case().p_name;
+
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    auto all = std::all_of(vec.begin(), vec.end(), [](const auto& elem) {
+        return elem > 0;
+    });
+
+    std::cout << "All elems are positive " << all << std::endl;
+
+    auto any = std::any_of(vec.begin(), vec.end(), [](const auto& elem) {
+        return elem < 0;
+    });
+
+    std::cout << "Has elem smaller than 0 " << any << std::endl;
+
+    auto none = std::none_of(vec.begin(), vec.end(), [](const auto& elem) {
+		return elem % 2 == 0;
+	});
+	log("Has no even elements: ", none, true);
+
+    std::cout << "Has no even elements " << none << std::endl;
 }
