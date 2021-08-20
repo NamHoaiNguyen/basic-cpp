065154e57557cafd06cf4bfc1f5ba16aa5d5d7ce
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2021-08-20 18:15:49 +0700

    rewrite cmake

---
+++ include/utils.h
@@ -0,0 +1,24 @@
+#ifndef INCLUDE_UTILS_H
+#define INCLUDE_UTILS_H
+
+/*Boost lib*/
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/framework.hpp>
+
+/*others*/
+#include <iostream>
+
+struct TestLog {
+	TestLog() {
+		std::cout << "\033[1m\033[32m---------- START TEST CASE: " << boost::unit_test::framework::current_test_case().p_name
+                  << "\033[1m\033[32m----------" << std::endl;
+	}
+	~TestLog() {
+		std::cout << "\033[1m\033[31m---------- FINISH TEST CASE: " << boost::unit_test::framework::current_test_case().p_name
+                  << "\033[1m\033[31m----------" << std::endl;
+	}
+};
+
+#define TEST_LOG() TestLog testLog
+
+#endif
--- src/stl/stl_test.cc
+++ src/stl/stl_test.cc
@@ -1,2 +1,11 @@
-#define BOOST_TEST_MODULE stl algorithms test
+#define BOOST_TEST_MODULE stl_algorithms_test
+
+#include "utils.h"
+
 #include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test_algorithm)
+{
+   TEST_LOG();
+    // std::cout << boost::unit_test::framework::current_test_case().p_name;
+}
