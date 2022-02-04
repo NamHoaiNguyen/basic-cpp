e38e7ea3204ebe91eccb3923b72d680a924aa3b1
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2022-02-04 18:08:50 +0700

    test hash join

--- src/basic-ds/hashjoin/main.cc
+++ src/basic-ds/hashjoin/main.cc
@@ -48,10 +48,16 @@ decltype(auto) join(const tab_t &tab1, int column_a, const tab_t &tab2, int colu
     for (size_t i = 0; i < tab2.size(); i++) {
         auto range = hashmap.equal_range(tab2[i][column_b]);
         for (auto it = range.first; it != range.second; it++) {
-            tab_t::value_type row;
-            row.insert(row.end() , tab1[it->second].begin() , tab1[it->second].end());
-            row.insert(row.end() , tab2[i].begin()          , tab2[i].end());
+            row_t row;
+            row.push_back(tab1[it->second][0]);
+            row.push_back(tab1[it->second][column_a]);
+            row.push_back(tab2[i][column_b]);
+            row.push_back(tab2[i][1]);
             res.push_back(std::move(row));
+    //         tab_t::value_type row;
+    //   row.insert(row.end() , tab1[it->second].begin() , tab1[it->second].end());
+    //   row.insert(row.end() , tab2[i].begin()          , tab2[i].end());
+    //   res.push_back(std::move(row));
         }
     }
 
